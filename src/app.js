156dac0e679e165438041d6f0e73b4a8e9225458
Tom Hinton <tomjhinton@gmail.com>
2019-11-14 14:08:56 +0000

    bg platform array

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ ctx.globalAlpha = 0.2
 ctx.fillStyle = 'green'
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
+var grd = ctx.createLinearGradient(0, 0, canvas.width, canvas.height)
+
 document.onkeydown = checkKey
 
 function checkKey(e) {
@@ -38,18 +40,72 @@ function checkKey(e) {
   }
 }
 
+
 let lastTime = 0;
 
+
+
+let player = {
+  height: 10,
+  width: 10,
+  posX: 0,
+  posY: 0,
+  velX: 3,
+  velY: 0
+
+}
+
+
+let world = {
+  gravity: 0.3,
+  friction: 0.3
+
+}
+
+
+var boxes = []
+
+// dimensions
+boxes.push({
+  x: 100,
+  y: 100,
+  width: 100,
+  height: 10
+})
+boxes.push({
+  x: 0,
+  y: 10 - 2,
+  width: 10,
+  height: 50
+})
+boxes.push({
+  x: 10 - 10,
+  y: 0,
+  width: 50,
+  height: 10
+})
+
+
 function gameLoop(timestamp) {
-  const deltaTime = timestamp - lastTime;
-  lastTime = timestamp;
+  const deltaTime = timestamp - lastTime
+  lastTime = timestamp
 
   ctx.clearRect(0, 0, 800, 600);
   ctx.globalAlpha = 0.2
-  ctx.fillStyle = 'green'
+  grd.addColorStop(0, '#8ED6FF')
+  grd.addColorStop(0.2, '#004CB3')
+  grd.addColorStop(0.4, '#EE4CB3')
+  grd.addColorStop(0.6, '#E000EE')
+  ctx.fillStyle = grd
   ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-  requestAnimationFrame(gameLoop);
+  ctx.fillStyle = 'black'
+  ctx.globalAlpha = 1
+  boxes.map(x => ctx.fillRect(x.y, x.x, x.width, x.height))
+
+  ctx.globalAlpha = 0.2
+
+  requestAnimationFrame(gameLoop)
 }
 
-gameLoop();
+gameLoop()
