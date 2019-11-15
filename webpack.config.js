b846f3c3da93b631ab874fedc825ef42e356079b
Tom Hinton <tomjhinton@gmail.com>
2019-11-15 23:20:02 +0000

    file path changed

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: 'file-loader',
+        loader: 'file-loader',
         options: {
           name: '[name].[ext]',
           outputPath: 'fonts/'
